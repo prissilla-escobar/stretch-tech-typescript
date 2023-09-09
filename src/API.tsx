889730f0656c6c8659739af5a2b77e9b3d13a889
Avery Berryman <averyberryman@gmail.com>
2023-09-09 11:40:18 -0600

    Add working fetch requests in API file

---
+++ src/API.tsx
@@ -0,0 +1,25 @@
+export const getPlayers= (): any => {
+    return fetch('https://free-nba.p.rapidapi.com/players?page=0&per_page=100', {
+      method: 'GET',
+	    headers: {
+            'X-RapidAPI-Key': 'bde1a9eea9mshf9e8a3f4895622ep165a4djsn96d27920d461',
+            'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+        }
+    })
+      .then(response => {
+        return response.json()
+      })
+  }
+
+  export const getTeams= (): any => {
+    return fetch('https://free-nba.p.rapidapi.com/teams', {
+      method: 'GET',
+	    headers: {
+            'X-RapidAPI-Key': 'bde1a9eea9mshf9e8a3f4895622ep165a4djsn96d27920d461',
+            'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+        }
+    })
+      .then(response => {
+        return response.json()
+      })
+  }
