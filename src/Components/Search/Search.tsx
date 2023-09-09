42701383c786dd7dcf7854d94a0e30874b0bc057
Prissilla Escobar <prissilla988@gmail.com>
2023-09-09 15:40:41 -0600

    Create Search component to be passed into the Header component. Imported dunk logo and created search input.

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,1 +1,15 @@
-export {}
+import './Search.css'
+import dunkLogo from '../../Assets/Jumpman_logo.svg.png'
+
+const Search: React.FC = () => {
+    return (
+        <div className="search-area">
+            <form>
+                <input type='search' id="searchInput" name="q" placeholder='Dunk in a Team Name to Find' />
+            </form>
+            <img className="dunk-logo" src={dunkLogo} alt="a black silouhette of a man dunking a basketball" />
+        </div>
+    )
+}
+
+export default Search
