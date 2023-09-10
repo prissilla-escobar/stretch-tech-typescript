877df7970f1ca677eb7f0fa2970bafbbe0454fc2
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-10 11:56:29 -0600

    Add AllTeams component to return

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import { getPlayers, getTeams } from './API'
 import Header from './Components/Header/Header'
+import AllTeams from './Components/AllTeams/AllTeams'
 
+type AppProps {
+  teams: Team[]
+}
+
+type Team {
+  
+}
 const App = () => {
 
   const [teams, setTeams] = useState([])
-  const [serverError, setServerError] = useState({error: false, message: ''})
+  const [serverError, setServerError] = useState({hasError: false, message: ''})
 
 
   useEffect(() => {
@@ -17,15 +25,18 @@ const App = () => {
         setTeams(data.data)
 
       })
-      .catch((error) => {
-
+      // we don't want to use :any here, how to fix?
+      .catch((error: any) => {
+        setServerError({hasError: true, message: `${error.message}`})
       })
+
   },[])
 
   return (
     <main>
       <Header />
       <div className='test'>BODY TEST</div>
+      <AllTeams teams={teams} />
     </main>
   )
 }
