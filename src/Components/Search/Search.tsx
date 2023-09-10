c3a31e887a981baef7b0cbfb45bf53ad1691a1e6
Prissilla Escobar <prissilla988@gmail.com>
2023-09-10 10:28:07 -0600

    Create Search component and add in placeholder for search bar

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,11 +1,11 @@
 import './Search.css'
 import dunkLogo from '../../Assets/Jumpman_logo.svg.png'
 
-const Search: React.FC = () => {
+const Search = () => {
     return (
         <div className="search-area">
             <form>
-                <input type='search' id="searchInput" name="q" placeholder='Dunk in a Team Name to Find' />
+                <input type='search' id="searchInput" name="q" placeholder='Search Team Name & Dunk >' />
             </form>
             <img className="dunk-logo" src={dunkLogo} alt="a black silouhette of a man dunking a basketball" />
         </div>
