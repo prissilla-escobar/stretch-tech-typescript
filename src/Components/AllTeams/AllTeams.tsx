9afa2853d4f45abce02d4166cc48b9a554d6151b
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-10 17:31:05 -0600

    Add props to pass to TeamCard

--- src/Components/AllTeams/AllTeams.tsx
+++ src/Components/AllTeams/AllTeams.tsx
@@ -1,4 +1,5 @@
 import './AllTeams.css'
+import TeamCard from '../TeamCard/TeamCard'
 
 type Team = {
   id: number;
@@ -15,17 +16,33 @@ type AllTeamsProps = {
 }
 const AllTeams = ({ teams }: AllTeamsProps) => {
   console.log('teams: ', teams)
+  const teamCards = teams.map((team) => {
+    return(
+      <TeamCard
+        key={team.id}
+        id={team.id}
+        abbreviation={team.abbreviation}
+        city={team.city}
+        conference={team.conference}
+        division={team.division}
+        full_name={team.full_name}
+        name={team.name}
+      />
+    )
 
+  })
   const firstTeam = teams[0]
 
   console.log('teams[0]: ', teams[0])
   return (
     <>
       
-      <h1 className='allTeams-cont'>Teams</h1>
-      <h2>{firstTeam.city}</h2>
+      {/* <h1 className='allTeams-cont'>Teams</h1>
+      <h2>{firstTeam.city}</h2> */}
+      <div className="teams-cont">{teamCards}</div>
     </>
   )
+
 }
 
 
