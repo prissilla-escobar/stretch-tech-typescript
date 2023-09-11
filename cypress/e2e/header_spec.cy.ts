4701ee31646a802afafe53d7515ed1f00444817d
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-11 17:55:27 -0600

    Add cypress testing intercept / stub

--- cypress/e2e/header_spec.cy.ts
+++ cypress/e2e/header_spec.cy.ts
@@ -3,3 +3,14 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
 })
+
+describe('header component', () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://free-nba.p.rapidapi.com/players?page=0&per_page=100", {
+      statusCode: 200,
+      fixture: "teamsData"
+    })
+    cy.visit('http://localhost:3000')
+  })
+  
+})
