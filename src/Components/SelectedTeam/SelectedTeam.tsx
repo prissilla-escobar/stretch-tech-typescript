84cd0f12145ac81450fab0e99c2c9876e360310c
Avery Berryman <averyberryman@gmail.com>
2023-09-11 18:57:55 -0600

    Add SelectedTeam component to filter players by team

--- src/Components/SelectedTeam/SelectedTeam.tsx
+++ src/Components/SelectedTeam/SelectedTeam.tsx
@@ -1,1 +1,44 @@
-export {}
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Team } from '../AllTeams/AllTeams';
+import { getPlayers } from '../../API'
+
+export type Player = {
+    id: number;
+    first_name: string;
+    last_name: string;
+    position: string;
+    team: Team;
+  };
+  
+  const SelectedTeam = () => {
+    const { teamId } = useParams<{ teamId: string }>();
+    const [players, setPlayers] = useState<Player[]>([]);
+  
+    useEffect(() => {
+      getPlayers()
+        .then((data: any) => {
+          const filteredPlayers = data.data.filter((player: Player) => player.team.id.toString() === teamId);
+          setPlayers(filteredPlayers);
+        })
+        .catch((error: any) => {
+          console.error("Failed to fetch players:", error);
+        });
+    }, [teamId]);
+  
+    return (
+      <div>
+        <h2 className='roster-container'>Current Roster</h2>
+        <ul className='roster-list'>
+        {players.map(player => (
+          <li key={player.id} className='player'>
+            {player.first_name} {player.last_name} - {player.position}
+          </li>
+        ))}
+        </ul>
+      </div>
+    );
+  }
+  
+
+export default SelectedTeam;
