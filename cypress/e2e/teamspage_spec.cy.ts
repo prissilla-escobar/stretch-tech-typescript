d8860998a0571fa5c069dd81109c0a2685e8fed6
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-11 18:16:10 -0600

    Add cypress test specs for other tests

--- cypress/e2e/teamspage_spec.cy.ts
+++ cypress/e2e/teamspage_spec.cy.ts
@@ -4,7 +4,7 @@ describe('template spec', () => {
   })
 })
 
-describe('header component', () => {
+describe('teams page on-load', () => {
   beforeEach(() => {
     cy.intercept("GET", "https://free-nba.p.rapidapi.com/players?page=0&per_page=100", {
       statusCode: 200,
@@ -19,5 +19,9 @@ describe('header component', () => {
 
   it('should show a collection of NBA teams', () => {
     cy.get('.teams-cont').find('.team-card').should('have.length', 30)
+  })
+
+  it('should show team details', () => {
+    cy.get('.team-card').first().contains('h3', 'Hawks')
   })
 })
