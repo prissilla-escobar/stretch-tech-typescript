f38ad2a067379eef5d60fdcb6a974746aaad225f
Prissilla Escobar <prissilla988@gmail.com>
2023-09-13 15:38:31 -0600

    Add Link to return home when clicked

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,16 @@
 import './Header.css'
 import Search from '../Search/Search'
 import nbaLogo from '../../Assets/NBA-logo-png.png'
+import { Link } from 'react-router-dom'
 
 
 const Header = () => {
     return (
         <header>
             <div className="page-name">
-                <h1>ROSTERWATCH</h1>
+                <Link to={'/'}>
+                    <h1>ROSTERWATCH</h1>
+                </Link>
                 <img className="nba-logo" src={nbaLogo} alt="a silouhette of a man running and bouncing a basketball with NBA next to him" />
             </div>
             <Search />
