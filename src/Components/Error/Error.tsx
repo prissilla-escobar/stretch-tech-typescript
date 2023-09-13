c6bbfda70662fef7d2231e95053b41dfcf76e347
Avery Berryman <averyberryman@gmail.com>
2023-09-12 20:37:13 -0600

    Modify Error component to take a type

--- src/Components/Error/Error.tsx
+++ src/Components/Error/Error.tsx
@@ -1,25 +1,32 @@
 import { NavLink } from "react-router-dom";
 import './Error.css';
 
-function ErrorComponent(props: any) {
+type ErrorProps = {
+    message?: string | { message: string };
+    resetError: () => void;
+  };
+
+  function ErrorComponent(props: ErrorProps) {
   const handleReset = () => {
     props.resetError();
   };
 
   return (
     <div className='error-msg'>
-      <h2>Uh-oh...That's an airball!</h2>
-      {!props.message ? (
-        <h2>The page you're looking for doesn't exist.</h2>
-      ) : (
-        <h2>{props.message.message}</h2>
-      )}
-      <NavLink to='/' className='nav'>
-        <div onClick={handleReset}>
-          <button className='back-button'>RosterWatch Home</button>
-        </div>
-      </NavLink>
+  <h2>Uh-oh...That's an airball!</h2>
+  <h2>
+    {!props.message 
+      ? "The page you're looking for doesn't exist." 
+      : typeof props.message === 'string'
+        ? props.message
+        : props.message.message}
+  </h2>
+  <NavLink to='/' className='nav'>
+    <div onClick={handleReset}>
+      <button className='back-button'>RosterWatch Home</button>
     </div>
+  </NavLink>
+</div>
   );
 }
 
