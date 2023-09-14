d792347e4b338d0c3f49ab6d772cb9463aa294dd
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-14 14:17:08 -0600

    Add all searchbar logic to Search component

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,13 +1,67 @@
+import { useState, useEffect } from 'react'
+import AllTeams from '../AllTeams/AllTeams'
 import './Search.css'
 import dunkLogo from '../../Assets/Jumpman_logo.svg.png'
 
-const Search = () => {
+export type Team = {
+  id: number;
+  abbreviation: string;
+  city: string;
+  conference: string;
+  division: string;
+  full_name: string;
+  name: string;
+}
+
+type AllTeamsProps = {
+  teams: Team[]
+}
+
+const initialFilteredTeams: Team[] = [];
+
+const Search = ({ teams }: AllTeamsProps) => {
+  const [filteredTeams, setFilteredTeams] = useState(initialFilteredTeams)
+
+  const [searchField, setSearchField] = useState('')
+
+  console.log('Searchteams: ', teams)
+// let filteredTeams = teams
+
+// searchField === '' ?  filteredTeams = teams : 
+
+const createFilteredTeams = () => {
+  let unfilteredTeams = [...teams]
+
+  const subsetTeams = unfilteredTeams.filter(team => {
+    return (
+      
+      team.city.toLowerCase().includes(searchField.toLowerCase()) ||
+      team.name.toLowerCase().includes(searchField.toLowerCase())
+      
+    )
+  })
+  setFilteredTeams(subsetTeams)
+}
+
+useEffect(() => {
+  createFilteredTeams()
+}, [searchField])
+
+  // const searchList = () => {
+  //   return (
+  //     <AllTeams filteredTeams={filteredTeams} />
+  //   )
+  // }
+
     return (
         <div className="search-area">
             <form>
-                <input type='search' id="searchInput" name="q" placeholder='Search Team Name & Dunk >' />
+                <input type='search' id="searchInput" name="q" placeholder='Search Team Name & Dunk >'
+                value={searchField}
+                onChange={event => setSearchField(event.target.value.trim)} />
             </form>
             <img className="dunk-logo" src={dunkLogo} alt="a black silouhette of a man dunking a basketball" />
+            <AllTeams filteredTeams={filteredTeams} />
         </div>
     )
 }
