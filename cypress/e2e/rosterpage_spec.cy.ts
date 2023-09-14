9df905e0b2c15be7d78cd7661b0262bb8b4e052e
Prissilla Escobar <prissilla988@gmail.com>
2023-09-14 14:22:57 -0600

    Add error tests and fix spacing

--- cypress/e2e/rosterpage_spec.cy.ts
+++ cypress/e2e/rosterpage_spec.cy.ts
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('selected team page on load', () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://free-nba.p.rapidapi.com/teams", {
+      statusCode: 200,
+      fixture: "teamsData"
+    }).visit('http://localhost:3000')
+
+    cy.intercept('GET', 'https://free-nba.p.rapidapi.com/players?page=0&per_page=100', {
+      statusCode: 200,
+      fixture: 'playersData'
+    }).as('playersApi')
   })
+
+  it('should get Atlanta Hawks and show details', () => {
+    cy.get('.team-card').first().click()
+      .url().should('eq', 'http://localhost:3000/team/1')
+  })
+
 })
