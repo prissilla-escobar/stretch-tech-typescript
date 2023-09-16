742c8ebc456daa73f1a45d4de9c39ab348911c95
Avery Berryman <averyberryman@gmail.com>
2023-09-16 12:41:44 -0600

    Fix bug to clear search cache if back/home button is clicked

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,29 +1,39 @@
-import './Header.css'
-import Search from '../Search/Search'
-import nbaLogo from '../../Assets/NBA-logo-png.png'
-import { Link, useLocation } from 'react-router-dom'
-
+import "./Header.css";
+import Search from "../Search/Search";
+import nbaLogo from "../../Assets/NBA-logo-png.png";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 type HeaderProps = {
-    setSearchTerm: (term: string) => void;
-  };
+  setSearchTerm: (term: string) => void;
+};
 
-const Header = ( { setSearchTerm }: HeaderProps) => {
-  const { pathname } = useLocation()
+const Header = ({ setSearchTerm }: HeaderProps) => {
+  const { pathname } = useLocation();
 
-  const isHomePage = pathname === '/'
-    return (
-        <header>
-            <div className="page-name">
-                <Link to={'/'}>
-                    <h1>ROSTERWATCH</h1>
-                </Link>
-                <img className="nba-logo" src={nbaLogo} alt="a silouhette of a man running and bouncing a basketball with NBA next to him" />
-            </div>
-            {isHomePage &&
-            <Search setSearchTerm={setSearchTerm} />}
-        </header>
-    )
-}
+  const isHomePage = pathname === "/";
 
-export default Header
+  useEffect(() => {
+    if (isHomePage) {
+      setSearchTerm("");
+    }
+  }, [isHomePage, setSearchTerm]);
+
+  return (
+    <header>
+      <div className="page-name">
+        <Link to={"/"}>
+          <h1>ROSTERWATCH</h1>
+        </Link>
+        <img
+          className="nba-logo"
+          src={nbaLogo}
+          alt="a silhouette of a man running and bouncing a basketball with NBA next to him"
+        />
+      </div>
+      {isHomePage && <Search setSearchTerm={setSearchTerm} />}
+    </header>
+  );
+};
+
+export default Header;
